6cec52119b479616546027076a9bf6644ae88d21
Paiva <danpaiva@live.com>
2025-11-02 14:16:58 -0300

    refact: Organizando a Program.cs

--- FeiraDeTrocaApi/FeiraDeTrocaApi/Program.cs
+++ FeiraDeTrocaApi/FeiraDeTrocaApi/Program.cs
@@ -2,9 +2,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using FeiraDeTrocaApi.Data;
 using FeiraDeTrocaApi.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
+
+builder
+    .Services
+    .AddDbContext<AppDbContext>(options =>
+    options
+    .UseSqlite(builder
+    .Configuration
+    .GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
